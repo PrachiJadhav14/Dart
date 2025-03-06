6371c5513b7fd92d0180b738c1d7bb9285f9f854
PrachiJadhav14 <prachijadhav5514@gmail.com>
2025-03-06 13:47:32 +0530

    Class_And_Object

--- Class_And_Object/02_Class_And_Object.dart
+++ Class_And_Object/02_Class_And_Object.dart
@@ -1,14 +1,18 @@
-class Student{
- String? name;
- String? address;
- int? mark;
+ class Student 
+ {
+    String? name;
+    String? address;
+    int? mark;
 
- String homework(String ass){
-  return "Assignment Compelte";
- }
-
-} 
-
-void main(){                              ///Entry Point Function
-  Student obj1 = new Student();
+    String homework(String ass) 
+      {
+         return "Assignment Compelte";
+      }
 }
+
+void main() 
+{
+    ///Entry Point Function
+    Student obj1 = new Student();
+}
+ 
