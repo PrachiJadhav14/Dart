1e549f5c73b77d1ed51e37595c0818c429cf2054
PrachiJadhav14 <prachijadhav5514@gmail.com>
2025-03-06 13:48:07 +0530

    Class_And_Object

--- Class_And_Object/03_Class_And_Object.dart
+++ Class_And_Object/03_Class_And_Object.dart
@@ -1,20 +1,21 @@
- class Student{
-  String? name;
-  String? address;
-  int? mark;
+class Student
+ {
+    String? name;
+    String? address;
+    int? mark;
+    static int? temp;
 
-  static int? temp;
-  
-  void prinStdInfo(){
-    print(this.name);
-    print(this.address);
-    print(this.mark);
+  void prinStdInfo() 
+  {
+      print(this.name);
+      print(this.address);
+      print(this.mark);
   }
- 
- 
- }
- void main(){
-  Student obj1 = new Student();
-  print(obj1.name);
-  Student.temp=10;
- }
+}
+
+void main() 
+{
+    Student obj1 = new Student();
+    print(obj1.name);
+    Student.temp = 10;
+}
