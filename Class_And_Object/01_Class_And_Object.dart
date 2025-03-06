d88137d9830bbb099c5200c00029a5563855527d
PrachiJadhav14 <prachijadhav5514@gmail.com>
2025-03-06 13:40:37 +0530

    Class_And_Object

--- Class_And_Object/01_Class_And_Object.dart
+++ Class_And_Object/01_Class_And_Object.dart
@@ -1,16 +1,17 @@
-
-
-void main(){                                     ///Entery Point Function
-           // class & Object 
-           //class
+void main() {
+  ///Entery Point Function
+  // class & Object
+  //class
 }
 
+class Home {
+  dynamic? vita;
 
-     class Home{
-            dynamic? vita;                            ///Character ,Data Member , Variable
-            dynamic? cement;
+  ///Character ,Data Member , Variable
+  dynamic? cement;
 
-            void compaundgate(dynamic vita,dynamic cement){         ///Behaviour,Member function,function
-              print("complete Gate And Compund");
-            }
-     }
+  void compaundgate(dynamic vita, dynamic cement) {
+    ///Behaviour,Member function,function
+    print("complete Gate And Compund");
+  }
+}
