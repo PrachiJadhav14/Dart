f56684e1713fb0a0015f67d6b38ac27fc9824020
PrachiJadhav14 <prachijadhav5514@gmail.com>
2025-03-06 13:48:58 +0530

    Class_And_Object

--- Class_And_Object/05_Class_And_Object.dart
+++ Class_And_Object/05_Class_And_Object.dart
@@ -1,29 +1,30 @@
-class Student{
-  String? name;
- String? address;
- int? mark;
+class Student 
+{
+    String? name;
+    String? address;
+    int? mark;
 
- static
- int? temp;
- String homework(String ass){
-  return "Asssignment ass";
- }
+    static int? temp;
 
- void printStdInfo(){
-  print(this.name);
-  print(this.address);
-  print(this.mark);
- }
+  String homework(String ass) 
+  {
+     return "Asssignment ass";
+  }
 
+  void printStdInfo()
+   {
+      print(this.name);
+      print(this.address);
+      print(this.mark);
+  }
 
- static void demo(){
+  static void demo() {}
+  void main() 
+  {
+      //Entry Point Function
+      Student obj1 = new Student();
+      obj1.name;
 
- }
- void main(){
-                              //Entry Point Function
-  Student obj1 = new Student();
-  obj1.name;
-
-  Student.temp;
- }
+      Student.temp;
+  }
 }
