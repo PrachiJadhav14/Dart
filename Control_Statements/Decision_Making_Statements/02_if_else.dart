f41dd78f1551df13b106b3bece03d22beb94b817
PrachiJadhav14 <prachijadhav5514@gmail.com>
2025-03-07 11:20:24 +0530

    Control_Statements

---
+++ Control_Statements/Decision_Making_Statements/02_if_else.dart
@@ -0,0 +1,19 @@
+void main()
+{
+  // int a = 10 , b = 20;
+  // if(a>b){
+  //   print("Yess");
+  // }
+  // else{
+  //   print("No");
+  // }
+
+  int No1 = 45 , No2 =50;
+  if(No1<No2){
+    print("Yess");
+  }
+  else{
+    print("No");
+  }
+}
+
