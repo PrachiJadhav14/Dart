8423481532c5054d20ca948b96254f6eb549e932
PrachiJadhav14 <prachijadhav5514@gmail.com>
2025-03-07 19:07:21 +0530

    Class_And_Object

---
+++ Dart_Program/Class_And_Object/06_Sum_Of_Two_No.dart
@@ -0,0 +1,17 @@
+ 
+ class Addition{
+   
+   add()
+   {
+     int a = 20 , b = 50, c = 0;
+      c = a+b; 
+      print(c);
+
+   }
+ }
+
+ void main()
+ {
+    Addition obj = Addition();
+     obj.add();
+ }
