2e902b398ea8fa57640a665f8109d4d0c22768cb
PrachiJadhav14 <prachijadhav5514@gmail.com>
2025-03-13 23:53:55 +0530

    Class_And_Object

--- Dart_Program/Class_And_Object/06_Sum_Of_Two_No.dart
+++ Dart_Program/Class_And_Object/06_Sum_Of_Two_No.dart
@@ -1,6 +1,5 @@
  
- class Addition{
-   
+ class calculation{
    add()
    {
      int a = 20 , b = 50, c = 0;
@@ -8,10 +7,45 @@
       print(c);
 
    }
+   sub(){
+    int a = 200, b = 100 , Sum = 0;
+    Sum = a - b;
+    print(Sum);
+   }
+   div(){
+    int a = 200, b = 100 , Sum = 0;
+    Sum = a * b;
+    print(div);
+   }
+
  }
 
  void main()
  {
-    Addition obj = Addition();
+        calculation obj = calculation();
      obj.add();
+     obj.sub();
+     obj.div();
  }
+
+
+
+
+
+// class Person {
+//   String name;
+//   int age;
+
+//   // Constructor using 'this' keyword
+//   Person(this.name, this.age);
+
+//   void display() {
+//     print("Name: ${this.name}");
+//     print("Age: ${this.age}");
+//   }
+// }
+
+// void main() {
+//   Person p1 = Person("Alice", 25);
+//   p1.display();
+// }
