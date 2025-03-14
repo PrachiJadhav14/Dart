70fab2c5b7f09ee22d7f40a3ee3e7f32bbd4ef71
PrachiJadhav14 <prachijadhav5514@gmail.com>
2025-03-14 23:35:20 +0530

    Class_And_Object

---
+++ Dart_Program/Class_And_Object/BAsic_Class_And_Object.dart
@@ -0,0 +1,13 @@
+class calculation {
+  add() {                                       ////function
+   
+    int a = 20, b = 12, c = 0;
+    c = a + b;
+    print(c);
+  }
+}
+
+void main() {                                   ///Entry Point Function                                        
+  calculation obj = calculation();              // class of  object
+  obj.add();                                     /// object  Class
+}
