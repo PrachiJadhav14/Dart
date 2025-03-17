94fd0d80dff723d0b78f7d4cccd3ea2c7cddae96
PrachiJadhav14 <prachijadhav5514@gmail.com>
2025-03-17 23:27:11 +0530

    Number.dart

---
+++ Dart_Program/Assignments/Data_Type_Program/Number.dart
@@ -0,0 +1,19 @@
+
+void main() {            ///Entry Point Function
+
+  // num Number = 1455;
+  // print(Number);
+  // print(Number.hashCode);     ///Virtual Address                   
+  // print(Number.runtimeType); //runtimeType
+
+  int  No1 =56 , No2 = 45;
+  int  sum =  No1 + No2 ;
+  print(sum);
+  print(No1.runtimeType);                         //runtimeType
+  print(No2.runtimeType);                        //runtimeType
+  print(sum.runtimeType);                       //runtimeType
+
+print(No1.hashCode);                         ///Virtual Address        
+print(No2.hashCode);                         ///Virtual Address        
+print(sum.hashCode);                         ///Virtual Address   
+}
