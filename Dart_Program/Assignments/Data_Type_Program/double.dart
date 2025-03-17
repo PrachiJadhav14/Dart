a8d8bb45696295be0c283dddd53ab70e168ec295
PrachiJadhav14 <prachijadhav5514@gmail.com>
2025-03-17 23:30:11 +0530

    double_Data_Program

---
+++ Dart_Program/Assignments/Data_Type_Program/double.dart
@@ -0,0 +1,8 @@
+void main(){                             //Enter Point Function
+   num Number = 14.050;
+
+   print(Number);
+   print(Number.runtimeType);            ////runtimetype
+   print(Number.hashCode);               ///Virtual Address
+
+}
