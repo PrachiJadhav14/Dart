0fc3c0fc541d3c55a659522f05c2631d306c1ed7
PrachiJadhav14 <prachijadhav5514@gmail.com>
2025-03-17 23:23:49 +0530

    Bool_Data_Type

---
+++ Dart_Program/Assignments/Data_Type_Program/Bool.dart
@@ -0,0 +1,8 @@
+ void main(){          ///Enter Point Function
+
+ bool Name = true;
+ print(Name);
+ print(Name.runtimeType);               //runtimetype
+ print(Name.hashCode);                  ////hashcode
+
+ }
