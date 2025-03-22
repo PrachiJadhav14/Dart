d076bdebc1e587294aaf26ba42c6e382e63e5541
PrachiJadhav14 <prachijadhav5514@gmail.com>
2025-03-22 22:58:07 +0530

    Function_Program

---
+++ Dart_Program/Function/Basic_Function_Program/Function_With_No_Arguments,Return_Type.dart
@@ -0,0 +1,30 @@
+// String ProductName(){
+//     return "Prachi Jadhav";
+// }
+  
+// void main(){
+//   String Game = ProductName();
+//   print(Game);
+// }
+
+
+
+// int add(){                     //Function               
+//   int No1 = 12;                  //Declaration
+//   int No2 = 12;                   //Declaration  
+//   return  No1+ No2;                //returnType           
+// }
+// void main(){                      //Entry Point Function
+// int Sum = add();               /////Function Call
+// print(Sum);
+
+// }
+
+
+String Name(){
+    return  "Hello Flutter";         
+}
+void main(){
+  String printName = Name();
+  print(printName);
+}
