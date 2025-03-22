c55fc70b9b28254bbe303ac2d1da0434e86e0621
PrachiJadhav14 <prachijadhav5514@gmail.com>
2025-03-22 22:59:46 +0530

    Basic_function

---
+++ Dart_Program/Function/Basic_Function_Program/No_Parameters_No_Return_Type.dart
@@ -0,0 +1,22 @@
+
+void ProductName(){
+  print("Prachi");         
+}
+void Productid(){
+  print(1455);
+}
+void main(){
+ ProductName();
+ Productid();
+}
+
+
+
+// void Name(){
+//   print("Prachi");        
+// }
+
+// void main(){
+//  Name();
+
+// }
