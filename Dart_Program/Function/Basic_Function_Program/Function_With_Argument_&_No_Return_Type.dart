478e74520a7da37d401f00938de987e7a8fe5633
PrachiJadhav14 <prachijadhav5514@gmail.com>
2025-03-22 22:55:46 +0530

    Function_Program

---
+++ Dart_Program/Function/Basic_Function_Program/Function_With_Argument_&_No_Return_Type.dart
@@ -0,0 +1,14 @@
+// void Sum (int No1 , int No2){
+//   print("The Sum is ${No1+No2}");
+// }
+
+// void main(){
+//   Sum(12, 34);
+// }
+
+String? StudentName(Name){
+  print("Product Full Name Is ${Name}");
+}
+ void main(){
+  StudentName("Prachi");
+ }
