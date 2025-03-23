da9a16d98e1be46611e634f19eafff9f6a81606f
PrachiJadhav14 <prachijadhav5514@gmail.com>
2025-03-23 16:16:17 +0530

    Basic_Dart

--- Dart_Program/Function/Basic_Function_Program/01_Basic_.dart
+++ Dart_Program/Function/Basic_Function_Program/01_Basic_.dart
@@ -1,25 +1,22 @@
-   
-   class myclass{
-    void printName (){          //Declaration
-      print('Hello Dart'.runtimeType);   ///runtimetypa of string
-      // print(myclass());
-    }
-
-    void mycar(){
-      String? name = "prachi";
-       //int? = 12;
-     
-    }
-      }
-  void main(){
-    print('Welcome To Dart');
-
-    var myC = myclass();
-    myC.printName(); //Function Call
-    print(myC.runtimeType);            //runtimetype of my class
-   
-  }
- 
-
-
+class myclass {
+  void printName() {    //Declaration
   
+    print('Hello Dart'.runtimeType);     ///runtimetypa of string
+
+   
+    // print(myclass());
+  }
+
+  void mycar() {
+    String? name = "prachi";           //int? = 12;
+
+  }
+}
+
+void main() {
+  print('Welcome To Dart');
+
+  var myC = myclass();
+  myC.printName();                    //Function Call
+  print(myC.runtimeType);             //runtimetype of my class
+}
