8b21f93a08e95e93acd067c29a2efe2b2cdf8826
PrachiJadhav14 <prachijadhav5514@gmail.com>
2025-03-25 18:25:32 +0530

    Basic_Function_Program

---
+++ Dart_Program/Function/Basic_Function_Program/02_Basic_Function_Program.dart
@@ -0,0 +1,9 @@
+
+
+void main(){         ///Entry Point Function
+    int res = add(10, 20);
+  }
+  int add (int a,int b){
+    return a+b;
+  }
+
--- Dart_Program/Function/Basic_Function_Program/Parameter_With_Return_Type.dart
+++ Dart_Program/Function/Basic_Function_Program/Parameter_With_Return_Type.dart
@@ -1,19 +1,19 @@
   
-  // int? Sum(int No1 , int No2){
-  //   return(No1+No2);
+  int? Sum(int no1 , int no2){     //Function Declaration
+    return(no1+no2);            
+  }
+
+void  main(){                     ///Entry Point Function
+    int? no = Sum(14,55);
+    print(no);                         ///Function Call
+  }
+
+
+  // String? StudentName(String Name,String Surname){
+  //   return(Name+Surname);
   // }
 
   // void main(){
-  //   int? No = Sum(14,55);
-  //   print(No);
+  // String? FullName  = StudentName("Prachi", "Jadhav");
+  // print(FullName);
   // }
-
-
-  String? StudentName(String Name,String Surname){
-    return(Name+Surname);
-  }
-
-  void main(){
-  String? FullName  = StudentName("Prachi", "Jadhav");
-  print(FullName);
-  }
