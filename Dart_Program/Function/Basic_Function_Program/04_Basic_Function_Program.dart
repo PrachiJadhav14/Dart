d23f9184495e7d188d7a06d0421c67709ced99d1
PrachiJadhav14 <prachijadhav5514@gmail.com>
2025-03-26 23:01:22 +0530

    Basic_Funtion_Program

---
+++ Dart_Program/Function/Basic_Function_Program/04_Basic_Function_Program.dart
@@ -0,0 +1,20 @@
+import '02_Basic_Function_Program.dart';
+
+class Student{
+  String? stdName;
+  int? rollNo;
+ 
+ Student(){
+  Student(String  stdName,int rollNo){
+
+  }
+ }
+}
+
+void main(){
+  int res = add(10, 20);
+}
+
+ind add(int a,int b){
+  return a+b;
+}
