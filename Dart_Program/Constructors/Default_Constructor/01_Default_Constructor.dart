f554330af67021ec68daea48629f187241f5f8ea
PrachiJadhav14 <prachijadhav5514@gmail.com>
2025-03-27 15:21:41 +0530

    Dafault_Contructor

--- Dart_Program/Constructors/Default_Constructor/01_Default_Constructor.dart
+++ Dart_Program/Constructors/Default_Constructor/01_Default_Constructor.dart
@@ -1,8 +1,9 @@
 import 'dart:io';
 
-class Addition
-{
-  Addition()                      ///Default Constructor
+class Addition {
+  Addition()             ///Default Constructor
+
+  
   {
     stdout.write("Enter First No => ");
     int No1 = int.parse(stdin.readLineSync()!);
@@ -10,21 +11,53 @@ class Addition
     stdout.write("Enter Second No => ");
     int No2 = int.parse(stdin.readLineSync()!);
 
-    int Sum=No1+No2;
+    int Sum = No1 + No2;
     print(Sum);
   }
-  sub()                                ///Function                 
+  substarction()         ///Function
+
+ 
   {
-    print("Sub");
+    stdout.write("Enter First No => ");
+    int No1 = int.parse(stdin.readLineSync()!);
+
+    stdout.write("Enter Second No => ");
+    int No2 = int.parse(stdin.readLineSync()!);
+
+    int Sub = No1 - No2;
+    print(Sub);
   }
-  div()                                    ///Function    
+
+  division()          ///Function 
+
+  
   {
-    print("Div");
+    stdout.write("Enter First No => ");
+    double No1 = double.parse(stdin.readLineSync()!);
+
+    stdout.write("Enter Second Number => ");
+    double No2 = double.parse(stdin.readLineSync()!);
+
+    double Div = No1 / No2;
+    print(Div);
+  }
+
+  Multiplication()       ///Function
+   {
+    stdout.write("Enter First No => ");
+    int No1 = int.parse(stdin.readLineSync()!);
+
+    stdout.write("Enter Second No => ");
+    int No2 = int.parse(stdin.readLineSync()!);
+
+    int Mult = No1 * No2;
+    print(Mult);
   }
 }
-void main()
-{
-  Addition obj=Addition();
-        obj.sub();                ///Object CAll
-        obj.div();
-  }
+
+void main() {
+  Addition obj = Addition();     ///Object CAll
+  obj.substarction();                //Function call
+  obj.division();                   //Function call
+  obj.Multiplication();              //Function call
+}
