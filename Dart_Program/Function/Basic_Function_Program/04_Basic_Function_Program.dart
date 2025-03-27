aa347d43846966407474b36b0111b3e940588b58
PrachiJadhav14 <prachijadhav5514@gmail.com>
2025-03-27 15:23:15 +0530

    Basic_Program

--- Dart_Program/Function/Basic_Function_Program/04_Basic_Function_Program.dart
+++ Dart_Program/Function/Basic_Function_Program/04_Basic_Function_Program.dart
@@ -1,18 +1,13 @@
-import 'dart:async';
 
-import '02_Basic_Function_Program.dart';
-
-class Student{
-  String? stdName;
+class Student{              ///Class Declaration 
+  String? stdName; 
   int? rollNo;
  
- Student(){            //// default  constructor
-   print("I am Default Constructor");
- }
- 
-                                 
+//  Student(){            //// default  constructor
+//    print("I am Default Constructor");
+//  }                              
   Student(String stdName,int rollNo){               //
-
+        print("i am parameterized constructor");
   }
  }
 
