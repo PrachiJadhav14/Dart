592ebcd88b959e138d13bb4b479ba0c947f0edcb
PrachiJadhav14 <prachijadhav5514@gmail.com>
2025-03-30 23:01:28 +0530

    Class_And_Object

--- Dart_Program/Class_And_Object/01_Class_And_Object.dart
+++ Dart_Program/Class_And_Object/01_Class_And_Object.dart
@@ -4,7 +4,7 @@ class Student {
   int? mark;
   static int? temp;
 
-  Student(String nm, String ad, int m)
+  Student(String nm, String ad, int m)             //parameterized constructor
 
   ///Constructor
   {
@@ -18,13 +18,22 @@ class Student {
     print(this.address);
     print(this.mark);
   }
+   void printfStdInfo() {
+    print(this.name);
+    print(this.address);
+    print(this.mark);
+  }
+ 
 }
 
 void main() {
   //Entry Point Function
   Student obj1 = new Student("prachi", "Malwadi", 70);
   obj1.name;
+  Student obj2 = new Student("sakshi", "Malwadi", 70);
+  obj2.name;
 
   Student.temp = 60;
+  print(Student.temp);
   obj1.printStdInfo();
 }
