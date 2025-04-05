ddb3b86ad7a5c62853aea2008ab0bf1e63c3db8e
PrachiJadhav14 <prachijadhav5514@gmail.com>
2025-04-05 23:33:25 +0530

    Basic_Program

---
+++ Dart_Program/Basic_Program/02_Basic.dart
@@ -0,0 +1,58 @@
+
+// class Test{
+//   Test(){                                   //Constructor
+//      print('in Test Constructore');           
+//   }
+// }
+// // void  Student(){                                  ///Function
+// //     print('Student Function');
+// //   }
+// void main(){
+//   // print(Test().runtimeType);                       ///Runtimetype
+//   print(  Test());
+ 
+// // Student();                                    //Function Call
+
+ 
+// }
+
+
+
+
+// class Test{
+//   Test(){                                   //Constructor
+//      print('in Test Constructore');           
+//   }
+// }
+
+// void main(){
+//   // print(Test().runtimeType);                       ///Runtimetype
+//   // print(  Test());
+//   Test obj = Test();
+//   print(Test);
+// }
+
+// class Product{
+//   Product (int a , int b){                    //   parameterized Constructo
+//    int sum = a+b;
+//    print(sum);
+//   }
+ 
+// }
+//  void main(){
+//     Product obj = Product(12, 21);       /////  Positional Parameter
+//   }
+
+
+ class Test{
+  Test(int Num1, int  Num2){
+        int Sum = Num1+Num2;
+      print (Sum);
+  }
+
+ 
+ }
+
+ void main(){
+  Test obj = Test( 12,  12);
+ }
