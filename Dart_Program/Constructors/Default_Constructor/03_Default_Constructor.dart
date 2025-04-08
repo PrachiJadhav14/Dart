2e8e5cb6861b367b1aebc2c2696438ea3f250406
PrachiJadhav14 <prachijadhav5514@gmail.com>
2025-04-08 23:32:34 +0530

    Default_Constructor

---
+++ Dart_Program/Constructors/Default_Constructor/03_Default_Constructor.dart
@@ -0,0 +1,28 @@
+
+
+// class Student{
+//   // String stdName;              //Declaration
+//   int? rollNo;                      //Declaration
+
+
+//   // Student(){}                /// Default Constructor/  No Parameter\\
+//   // Student(string stdName,int rollNo){
+             
+//   // }
+
+ 
+// }  
+// void main(){                             ///entry point function
+
+//   int res = add(10, 20);
+ 
+//   print(res);
+//   print(res.runtimeType);
+
+// }
+//  int add(int a,int b){                     //parameterised constructor           
+//   return a+b;
+
+  
+
+// }
