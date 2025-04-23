72f857eb79b242b6cb53ab1583b65fa0ae1edf4e
PrachiJadhav14 <prachijadhav5514@gmail.com>
2025-04-23 19:27:56 +0530

    Multilevel_Inheritance

--- Dart_Program/Assignments/04_Inheritance/02_Types_Of_Inheritance/02_Multilevale_Inheritance.dart
+++ Dart_Program/Assignments/04_Inheritance/02_Types_Of_Inheritance/02_Multilevale_Inheritance.dart
@@ -1,22 +1,16 @@
-
 import 'dart:io';
 class Hotel{
   Hotel(){
     String? Name ="Vithal Kamat Hotel";        //Declaration
     String? Location = "Karad";                 //Declaration
-
     stdout.write("\n=========================********************====================\n");
-
     stdout.writeln("Welcome To ${Name}");
     stdout.write("Youre Locataion is ${Location}");
-
     stdout.write("\n=====================***************==================\n");
   }
-  
   }
   class Product extends Hotel{
     Product(){                                         ///Default Constructor
-      
       stdout.writeln("\n Enter You're Product Menu => \n");
       stdout.writeln("\n\t 1. Noodles");
       stdout.writeln("\n\t 2. Cold Drinks");
@@ -24,29 +18,26 @@ class Hotel{
       stdout.writeln("\n\t 4. Desert ");  
       stdout.write("\n=====================***************==================\n");
     }
-   
   }
   class Menu extends Product{
     String? Company;
       String? Name;
       double? Price;
       
-  void info() {                                          //Get Function
+  void info() {                                          //info Function
     stdout.writeln("\nEnter Product Company Name is =>");
     Company = stdin.readLineSync()!;
-     stdout.writeln("Enter Product  Name is =>");
+     stdout.writeln(" Enter Product  Name is =>");
      Name = stdin.readLineSync()!;
     stdout.writeln("Enter Product Price is =>");
      Price = double.parse(stdin.readLineSync()!);
   }
-
-  void printInfo() {                                        /////Set Function
+  void printInfo() {                                        /////printInfo Function
     stdout.writeln("\n Product Company Name is ${Company}");
     stdout.writeln("Product  Name is ${Name}");
     stdout.writeln("Product Price is ${Price}\n");
   }
   }
-
   void main(){
     Menu obj = Menu();                           ///Obj Call
     obj.info();                                  
