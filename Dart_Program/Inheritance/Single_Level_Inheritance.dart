3f8bf2395bcd81078fdc3b045d6cfbe7902fd3a7
PrachiJadhav14 <prachijadhav5514@gmail.com>
2025-05-19 12:20:46 +0530

    Single_Level_Inheritance

--- Dart_Program/Inheritance/Single_Level_Inheritance.dart
+++ Dart_Program/Inheritance/Single_Level_Inheritance.dart
@@ -1,22 +1,21 @@
-// class ArithematicCalculation{
-//   int addofAllNo(int No1,int No2,int No3){
-//     return No1+No2+No3;
-//   }
-//   int SubofAllNo(int No1, int No2, int No3){
-//     return  No1-No2-No3;
-//   }
-// }
+class ArithematicCalculation{
+  int addofAllNo(int No1,int No2,int No3){
+    return No1+No2+No3;
+  }
+  int SubofAllNo(int No1, int No2, int No3){
+    return  No1-No2-No3;
+  }
+}
+class Calculator extends ArithematicCalculation{
+    int? No1 = 0;
+    int? No2 = 0;
+    int? No3 = 0;
+    Calculator(this.No1,this.No2,this.No3);
+}
 
-// class Calculator extends ArithematicCalculation{
-//     int? No1 = 0;
-//     int? No2 = 0;
-//     int? No3 = 0;
-//     Calculator(this.No1,this.No2,this.No3);
-// }
-
-// void main(){
-//   Calculator obj1 = Calculator(12, 12, 12);
-//   //  
-// }
+void main(){
+  Calculator obj1 = Calculator(12, 12, 12);
+  //  
+}
 
 
