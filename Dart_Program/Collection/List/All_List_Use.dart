62a351b636fb0b34456fd93d4cadabdc3e7971a5
PrachiJadhav14 <prachijadhav5514@gmail.com>
2025-05-19 20:28:34 +0530

    Collection_Assignment

---
+++ Dart_Program/Collection/List/All_List_Use.dart
@@ -0,0 +1,78 @@
+
+import 'dart:io';
+
+void main(){
+//   1)
+//   List<int>listNo = [1,2,3,4,5];
+//   listNo.add(6);
+//   print(listNo);
+
+// 2)
+  // List<int>listNum = [1,2,3,4,5,6];
+  // listNum.remove(3);
+  // print(listNum);
+
+// 3)
+// List<String>listString=['A','B','C','D'];
+// print(listString.length);
+
+
+  //  4)
+//   List<int>listno=[1,2,3,4,5,6];
+//  print(listno.length);
+ 
+
+// 5)
+//  List<int>listNo = [1,2,3,4,5];
+//    listNo.removeAt(2);
+//   print(listNo);
+
+  // 6)
+  // List<int>listNo = [1,2,3,4,5];
+  //  print(listNo.contains(8));                                    //false
+
+
+// 7)
+// List<int>listNo = [1,2,3,4,5];
+//    print(listNo.contains(4));                                      //true
+
+
+// 8)
+// List<int>listno = [1,2,3,4,5];
+//  print(listno.isNotEmpty);
+
+// 9)
+// List<int>listno = [];
+// List<int>listno = [1,2,3,4,5];
+//  print(listno.isEmpty);
+
+//  10)
+// List<int>listno = [1,2,3,4,5,6];
+// print(listno.insert(1,2));
+
+
+// 11)
+// List<int>listno = [1,2,3,4,5,6];
+// listno.insertAll(12, 2);
+
+
+// 12)
+// List<String>listString = ['a','b','c','d'];
+// print(listString.reversed);
+
+// 13)
+// List<String>listString=['A','B','C','D'];
+// print(listString.firstOrNull);
+
+// 14
+List<String>listString=['A','B','C','D'];
+print(listString.indexed);
+
+// 15)
+List<String>liststring=['A','B','C','D'];
+print(liststring.iterator);
+
+}
+
+
+
---
+++ Dart_Program/Collection/Set/Sets.dart
@@ -0,0 +1,62 @@
+import 'dart:io';
+
+class Test{
+  add(){
+
+    // 1)
+      //  Set items={1,2,3,4,5,6};
+      //  items.add(7);
+      //   print(items);  
+
+// 2)
+    //  Set items={};
+    // print(items.isEmpty);
+
+  //  3)
+  //  Set items={1,2,3,4,5,6};
+  //  print(items.isNotEmpty);
+
+    // 4)
+    // Set items={1,2,3,4,5,6};
+    // print(items.first);
+
+    //  5)
+    // Set items={1,2,3,4,5,6};
+    // print(items.last);
+
+    //  6)
+    // Set items={1,2,3,4,5,6};
+    // print(items.length);
+
+   // 7)
+        //   Set items={1,2,3,4,5,6};
+        // print(items.contains(4));
+
+
+        // 8)
+        //  Set items={1,2,3,4,5,6};
+        //  print(items.elementAt(0));
+        // print(items.elementAt(2));
+          
+    // 9)
+    // Set items= {1,2,3,4,5};
+    // print(items.indexed);
+
+    // 10)
+    // Set items ={'a','b','c','d','e','f','f','c','a','b','c','d','e',};
+    // print(items.nonNulls);
+
+
+    // 11)
+    // Set items= {1,};
+    // print(items.singleOrNull);
+
+  // 12)
+    Set items= {1,2,3,4,5,6};
+    print(items.singleOrNull);
+  }
+}
+void main(){
+  Test obj = Test();
+  obj.add();
+}
