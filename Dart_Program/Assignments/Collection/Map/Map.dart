f6613d0e8e63111e8c02b2a6dfe3c5e8de7fc103
PrachiJadhav14 <prachijadhav5514@gmail.com>
2025-05-19 20:30:25 +0530

    Map_Assignment

---
+++ Dart_Program/Assignments/Collection/Map/Map.dart
@@ -0,0 +1,108 @@
+void main(){
+  // 1)
+  // Map<String,String>map ={
+  //                 "Emp_Name": "Prachi Jadhav",
+  //                 "Emp_Id" : "1455"  
+  // };
+
+  // print('${map['Emp_Name']}');
+
+  // 2)
+  // Map<int,int>map ={
+  //                 1:14,
+  //                 2:55   
+  // };
+  //  print(map);
+
+// 3)
+  //  Map<double,double>map={
+  //         0.1:12,
+  //         0.2:23.23
+  //  };
+  //  print(map);
+
+
+  // 4)
+  //  Map<int,double>map={
+  //         1:12,
+  //         2:23.23
+  //  };
+  //  print(map);
+   
+   
+  //  5)
+  //  Map<int,dynamic>map={
+  //         1:12,
+  //         2:23.23,
+  //         1:23
+  //  };
+  //  print(map.runtimeType);
+  //   print(map);
+
+
+  // 6)
+  // Map<String,dynamic>map={
+  //         "Id":1455,
+  //         "Name":"Prachu",
+          
+  //  };
+  //  map["Collage"] ="YCCSK";
+  //  print(map);
+
+    
+    // 7)
+  //  Map<String,dynamic>map={
+  //          "Id":1455,
+  //         "Name":"Prachu",
+          
+  // };
+  //  map["Collage"] ="YCCSK";
+  // print(map.keys);
+
+  // 8)
+  // Map<String,dynamic>map={
+  //          "Id":1455,
+  //         "Name":"Prachu",
+          
+  // };
+  //  map["Collage"] ="YCCSK";
+  // print(map.values);
+
+  // 9)
+  // Map<String,dynamic>map={
+  //          "Id":1455,
+  //         "Name":"Prachu",
+  // };
+  //  map["Collage"] ="YCCSK";
+  // print(map.length);
+
+  // 10)
+  // Map<String,dynamic>map={
+  //          "Id":1455,
+  //         "Name":"Prachu",
+          
+  // };
+  //  map["Collage"] ="YCCSK";
+  // print(map.isNotEmpty);
+
+
+
+// 11)
+  // Map<String,dynamic>map={
+  //          "Id":1455,
+  //         "Name":"Prachu",
+          
+  // };
+  //  map["Collage"] ="YCCSK";
+  // print(map.remove('Id'));
+
+  // 12
+
+  Map<String,dynamic>map={
+           "Id":1455,
+          "Name":"Prachu",
+          
+  };
+   map["Collage"] ="YCCSK";
+  print(map.entries);
+}
